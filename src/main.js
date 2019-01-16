0c416ea7b76e1a5fe9cf097a0d0431b6615b1933
camilaAstorgaBatarce <camilaastorga03@gmail.com>
2019-01-16 11:47:48 -0300

    main.js/ se añadio la const champion bosx que muestra la card con los campeones de lol y sus nombres

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
-const dataLol = LOL.data;
+window.onload = () => {
 
+    const dataLol = Object.entries(LOL.data);
 // traer los elementos filtrados de los tipos de champion y mostrarlos en el html
+    let fillWithchampion = (champions) => {
+        document.getElementById("root").innerHTML = "";
+        filtered.forEach(elementChampion =>{
+            document.getElementById("root").innerHTML += championBox(dataLol[i].name, dataLol[i].key, dataLol[i].tags, datalol[i].img);
+        })
+    }
+}
+// al ocupar root se trae los elementos de championbox
+    const championBox = document.getElementById('root');
+    document.getElementById('select').addEventListener('change', () => {
+        let selectValue = document.getElementById('select').value;
+        let filteredChamps = filter(dataLol,selectValue);
+        championBox.innerHTML = "";
+        filteredChamps.forEach(elementChampion => {
+            championBox.innerHTML += `<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+        <div class="card w-100 champion-box">
+        <div  class="card-img-top">
+        <img class="img-fluid" src="${elementChampion[1].splash}" alt="imagen de campeón">
+        </div>
+        <div class="card-body">
+        <p class="card-text">Nombre:${elementChampion[1].name}</p>
+        <p class="card-text">${elementChampion[1].title}</p>
+        </div>
+        </div>
+        </div>`;
+            })
+            // return filteredData;
+        })
